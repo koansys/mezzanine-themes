bdc930852af39053339b1e5bbb7aca6ab503afa8
Josh Finnie <josh@koansys.com>
2012-08-03 17:11:42 -0400

    updating more tweeks to new indypub theme

---
+++ themes/mezzanine_indypub/static/js/general.js
@@ -0,0 +1,51 @@
+$(function(){
+	$("#slideshow").craftyslide();
+});
+
+function validateNewsletterSignUp(form) {	
+	var errors = 0;
+
+	var hasValue = $("#newsletterForm #emailAddress").attr("value");		
+	
+	if (hasValue != "") {
+		errors = errors;
+	}
+	else {
+		errors = errors + 1;
+	}
+	
+	
+	if (errors >= 1) {
+		$("#newsletterForm #emailAddress").addClass("error");
+		
+		return false;
+	}
+	else {
+		//hide the form			
+		$("#newsletterForm").hide();
+		$("#loader").show();
+
+		$("#newsletterForm #emailAddress").removeClass("error");
+
+		//send the ajax request  
+		$.get('../static/php/newsletterSignUp.php',{emailAddress:$("#newsletterSignUp #emailAddress").val()}, 		
+		
+		//return the data
+		function(data){
+		
+			setTimeout('returnData()', 500);
+		});	
+		
+				
+		return false;
+	}
+
+	
+}
+
+function returnData() {
+	//hide the graphic
+	$("#loader").hide();
+	$("#submitted").show();
+}
+			
