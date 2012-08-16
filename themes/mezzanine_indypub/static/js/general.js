1e6f59458390b175f38fe735c22aaec58cf7eb4a
Mckenzie Tully <mckenzie@koansys.com>
2012-08-16 09:06:47 -0500

    CSS fix for top header links, initial add of script for navigation
    Initially adding js to identify for the CSS when a certain top-level page is selected

--- themes/mezzanine_indypub/static/js/general.js
+++ themes/mezzanine_indypub/static/js/general.js
@@ -1,5 +1,29 @@
 $(function(){
 	$("#slideshow").craftyslide();
+	
+	//Go through each navigation Item
+	$("#navigation ul li a").each(function() {
+		var currentURL = window.location.toString();
+		var navigationLocation = $(this).attr("href");
+				
+		//If the navigation item url contains text from the current URL
+		if (currentURL.indexOf(navigationLocation) != -1) {
+			$(this).parent().addClass("nav-current");
+			
+			var navigationLocationSubStr = navigationLocation.substring(0, navigationLocation.length-5);
+			
+			$("#largePicture").addClass(navigationLocationSubStr);
+		}
+		
+		//If the URL is the root
+		else if ( $("#header").hasClass("homepage")) {
+			$("#navigation ul li:first-child a").addClass("selected");
+			$("#largePicture").addClass("home");
+		}
+	});
+	
+	$("#awardsChart tr:odd td").addClass("lastrow");
+
 });
 
 function validateNewsletterSignUp(form) {	
